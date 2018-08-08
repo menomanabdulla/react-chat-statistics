9e4560e6325619af58196fc08212ac3fca2729ef
nomanabdulla <noman.fcibappy@gmail.com>
2018-08-09 05:29:41 +0600

    add active friend and deactive friend module

--- src/component/App.js
+++ src/component/App.js
@@ -1,18 +1,61 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
+function ActiveFrndComp(props){
+  return(
+    <div>
+         <h1>Active Friend</h1>
+        <ul>
+          {
+            props.activeList.map((name,key) =>(
+              <li key={key}>
+                <span>{name}</span>
+                <button onClick={()=>props.deactiveFriend(name)}>Deactive</button>
+                <button onClick={()=>props.removeFriend(name)}>Remove</button>
+              </li>
+            ))
+          }
+        </ul>
+    </div>
+  )
+}
+
+function DeactiveFrndComp(props){
+  return(
+    <div>
+         <h1>Deactive Friend</h1>
+        <ul>
+          {
+            props.deactiveList.map((name,key) =>(
+              <li key={key}>
+                <span>{name}</span>
+                <button onClick={()=>props.activeFriend(name)}>Active</button>
+              </li>
+            ))
+          }
+        </ul>
+    </div>
+  )
+}
+
 class App extends Component {
   constructor(props){
-    render(props)
+    super(props)
     this.state= {
       activeFrnd:[
-
+        'noman','abdulla'
       ],
       deactiveFrnd:[
 
       ],
       inputValue: ''
     }
+    this.inputHandeler = this.inputHandeler.bind(this)
+    this.submitHandeler = this.submitHandeler.bind(this)
+    this.removeHandler = this.removeHandler.bind(this)
+    this.deactiveHandeler = this.deactiveHandeler.bind(this)
+    this.activeHandeler = this.activeHandeler.bind(this)
   }
  
   inputHandeler(e){
@@ -26,19 +69,50 @@ class App extends Component {
     e.preventDefault();
     this.setState({
       ...this.state,
-      activeFrnd: this.state.activeFrnd.concat(this.state.inputValue),
+      activeFrnd: this.state.activeFrnd.concat([this.state.inputValue]),
       inputValue: ''
     })
+    console.log(this.state.activeFrnd)
   }
+ 
+  deactiveHandeler(name){
+    this.setState({
+      ...this.state,
+      activeFrnd: this.state.activeFrnd.filter(item => item !== name ),
+      deactiveFrnd: this.state.deactiveFrnd.concat([name])
+    })
+  }
+  activeHandeler(name){
+    this.setState({
+      ...this.state,
+      activeFrnd: this.state.activeFrnd.concat([name]),
+      deactiveFrnd:  this.state.deactiveFrnd.filter(item => item !== name )
+     
+    })
+  }
+  removeHandler(name){
+    this.setState({
+      ...this.state,
+      activeFrnd: this.state.activeFrnd.filter(item => item !== name )
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <div className='add-block'>
-          <form onSubmit={()=>this.submitHandeler()}>
-
-            <input type="text" value={this.state.inputValue}  onChange={()=>this.inputHandeler()} />
-
+          <form onSubmit={this.submitHandeler}>
+            <input type="text" value={this.state.inputValue}  onChange={this.inputHandeler} />
+            <button type="submit" className="btn tbn-default">Add Friend</button>
           </form>
+          <div>
+              <ActiveFrndComp activeList = {this.state.activeFrnd}
+                removeFriend = {this.removeHandler}
+                deactiveFriend = {this.deactiveHandeler}
+              />
+              <DeactiveFrndComp deactiveList = {this.state.deactiveFrnd}
+                activeFriend = {this.activeHandeler} />
+          </div>
         </div> 
       </div>
     );
